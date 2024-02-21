ac58dff51deb63a3a60749ca4e5ea2c861ab5e36
Daniel Wieczorek <121404035+byte4RR4Y@users.noreply.github.com>
2024-02-21 12:52:54 +0100

    Add files via upload

--- scripts/installkernel.sh
+++ scripts/installkernel.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-ARCHIVE="$1"
-ARCHIVE="$(sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' <<< "${ARCHIVE}")"
+ARCHIVE=$1
 
-unzip -o "${ARCHIVE}" -d / || errexit "unzip ${ARCHIVE} failed"
-BUILD="$(unzip -l "${ARCHIVE}" boot/vmlinuz-* | sed -n 's|^.*boot/vmlinuz-\(.*\)$|\1|p')"
+BUILD="$(unzip -l ${ARCHIVE} boot/vmlinuz-* | sed -n 's|^.*boot/vmlinuz-\(.*\)$|\1|p')"
+unzip -o "${ARCHIVE}" -d /
 update-initramfs -c -v -k "${BUILD}"
-echo "Kernel installation completed"
+
--- scripts/makekernel.sh
+++ scripts/makekernel.sh
@@ -2,43 +2,58 @@
 
 BRANCH=$1
 CWD=$PWD
+OUTDIR=${CWD}
 CPUS=$(($(nproc)))
-KERNELDIR="KERNEL"
-build_version=$(sed -n '3p' .config)
-KERNELVERSION=$(echo "$build_version" | awk '{print $3}')
-OUTDIR=KERNEL-${KERNELVERSION}
-CONFIG_FILE=".config"
-NEW_VALUE='CONFIG_LOCALVERSION="-byte4rr4y"'
 
 git clone --depth=1 --branch $BRANCH https://github.com/raspberrypi/linux
 cd linux
-KERNEL=kernel_2712
+
 make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- bcm2712_defconfig
-sed -i "s/^CONFIG_LOCALVERSION=.*$/$NEW_VALUE/" "$CONFIG_FILE"
+
+KERNELDIR="KERNEL-${BRANCH}"
 mkdir -p "${KERNELDIR}"
-make -j "${CPUS}" KERNELRELEASE="${KERNELVERSION}" ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- Image.gz modules dtbs
-env PATH=$PATH make KERNELRELEASE="${KERNELVERSION}" ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- INSTALL_MOD_PATH="${KERNELDIR}" modules_install
-mkdir -p "${KERNELDIR}/boot/firmware/"
-mkdir -p "${KERNELDIR}/lib/linux-image-${KERNELVERSION}/broadcom/"
-mkdir -p "${KERNELDIR}/lib/linux-image-${KERNELVERSION}/overlays/"
-echo "ffffffffffffffff B The real System.map is in the linux-image-<version>-dbg package" > "${KERNELDIR}/boot/System.map-${KERNELVERSION}"
-cp .config "${KERNELDIR}/boot/config-${KERNELVERSION}"
-cp arch/arm64/boot/Image.gz "${KERNELDIR}/boot/vmlinuz-${KERNELVERSION}"
-cp arch/arm64/boot/Image.gz "${KERNELDIR}/boot/firmware/${OLDIMG}"
-cp arch/arm64/boot/dts/broadcom/*.dtb "${KERNELDIR}/lib/linux-image-${KERNELVERSION}/broadcom/"
-cp arch/arm64/boot/dts/overlays/*.dtb* "${KERNELDIR}/lib/linux-image-${KERNELVERSION}/overlays/"
-cp arch/arm64/boot/dts/overlays/README "${KERNELDIR}/lib/linux-image-${KERNELVERSION}/overlays/"
 
-# Erstellen des Zip-Archivs
-ARCHIVE="kernel-$(sed -n 's|^.*\s\+\(\S\+\.\S\+\.\S\+\)\s\+Kernel Configuration$|\1|p' .config)$(sed -n 's|^CONFIG_LOCALVERSION=\"\(.*\)\"$|\1|p' .config).zip"
-cd "${KERNELDIR}"
-find lib -type l -exec rm {} \;
-zip -q -r "${ARCHIVE}" *
-cd -
+make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- bcm2712_defconfig
+sed -i "s/^CONFIG_LOCALVERSION=.*$/CONFIG_LOCALVERSION="-byte4rr4y"/" ".config"
+BUILD="$(sed -n 's|^.*\s\+\(\S\+\.\S\+\.\S\+\)\s\+Kernel Configuration$|\1|p' .config)"
 
-mkdir $OUTDIR
-mv "${KERNELDIR}/${ARCHIVE}" "${CWD}"
-rm -rf $OUTDIR
-cd $CWD
+  #KERNELDIR="$(mktemp --directory --tmpdir build-kernel-KERNELDIR-XXXXX)"
 
+  
+      make -j ${CPUS} KERNELRELEASE="${BUILD}" ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- Image.gz modules dtbs
+      env PATH=$PATH make KERNELRELEASE="${BUILD}" ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- INSTALL_MOD_PATH=${KERNELDIR} modules_install
+      mkdir -p "${KERNELDIR}/boot/firmware/"
+      mkdir -p "${KERNELDIR}/lib/linux-image-${BUILD}/broadcom/"
+      mkdir -p "${KERNELDIR}/lib/linux-image-${BUILD}/overlays/"
+      echo "ffffffffffffffff B The real System.map is in the linux-image-<version>-dbg package" > "${KERNELDIR}/boot/System.map-${BUILD}"
+      cp .config "${KERNELDIR}/boot/config-${BUILD}"
+      cp arch/arm64/boot/Image.gz "${KERNELDIR}/boot/vmlinuz-${BUILD}"
+      cp arch/arm64/boot/Image.gz "${KERNELDIR}/boot/firmware/kernel_2712.img"
+      cp arch/arm64/boot/dts/broadcom/*.dtb "${KERNELDIR}/lib/linux-image-${BUILD}/broadcom/"
+      cp arch/arm64/boot/dts/overlays/*.dtb* "${KERNELDIR}/lib/linux-image-${BUILD}/overlays/"
+      cp arch/arm64/boot/dts/overlays/README "${KERNELDIR}/lib/linux-image-${BUILD}/overlays/"
+   
+  ARCHIVE="kernel-$(sed -n 's|^.*\s\+\(\S\+\.\S\+\.\S\+\)\s\+Kernel Configuration$|\1|p' .config)$(sed -n 's|^CONFIG_LOCALVERSION=\"\(.*\)\"$|\1|p' .config).zip"
+  cd "${KERNELDIR}"
+  find lib -type l -exec rm {} \;
+  zip -q -r "${ARCHIVE}" *
+  if [ "${OUTDIR}" != "" ]; then
+    if [ "${OUTDIR: -1}" != "/" ]; then
+      OUTDIR+="/"
+    fi
+  else
+    if [ "${REALUSER}" = "root" ]; then
+      OUTDIR="/root/"
+    else
+      OUTDIR="/home/${REALUSER}/"
+    fi
+  fi
+chown "${REALUSER}:${REALUSER}" "${ARCHIVE}"
+cd ${CWD}/linux
+mv "${KERNELDIR}/${ARCHIVE}" "${OUTDIR}"
+cd "${CWD}"
+rm -r "${KERNELDIR}"
+
+
+echo "1" > ${CWD}/config/kernel_status
 echo "1" > ${CWD}/config/kernel_status
